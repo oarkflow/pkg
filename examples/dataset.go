ee9264c8c03b1bee484a467e678afee6ec460498
sujit <s.baniya.np@gmail.com>
2024-03-09 18:39:54 +0545

    update: add `GroupBy`

--- examples/dataset.go
+++ examples/dataset.go
@@ -6,12 +6,17 @@ import (
 
 	"github.com/oarkflow/pkg/dataset"
 	"github.com/oarkflow/pkg/rule"
+	"github.com/oarkflow/pkg/sliceutil"
 )
 
 func main() {
+	groupBy()
+}
+
+func datasetCheck() {
 	// ruleCheck()
 	df := dataset.LoadMaps(
-		[]map[string]interface{}{
+		[]map[string]any{
 			{"id": 5, "code": "BJS", "name": "CN", "money": 1.23},
 			{"id": 2, "code": "BJS", "name": "CN", "money": 2.21},
 			{"id": 3, "code": "SHA", "name": "CN", "money": 1.26},
@@ -24,6 +29,37 @@ func main() {
 	for key, group := range groups.GetGroups() {
 		fmt.Println(key, group.Maps())
 	}
+}
+
+type Person struct {
+	Name    string
+	Age     int
+	Country string
+}
+
+func groupBy() {
+	people := []Person{
+		{Name: "Alice", Age: 30, Country: "USA"},
+		{Name: "Bob", Age: 25, Country: "Canada"},
+		{Name: "Charlie", Age: 30, Country: "USA"},
+		{Name: "David", Age: 35, Country: "Canada"},
+	}
+	persons := []map[string]any{
+		{"name": "Alice", "age": 30, "country": "USA"},
+		{"name": "Bob", "age": 25, "country": "Canada"},
+		{"name": "Charlie", "age": 30, "country": "USA"},
+		{"name": "David", "age": 35, "country": "Canada"},
+	}
+	employees := []map[string]any{
+		{"name": "Alice", "country": "USA"},
+		{"name": "Bob", "country": "Canada"},
+		{"name": "Charlie", "country": "USA"},
+		{"name": "David", "country": "Canada"},
+	}
+	// Use GroupBy with Person type and key function
+	fmt.Println(sliceutil.GroupBy(people, "Age", "Country"))
+	fmt.Println(sliceutil.GroupBy(persons, "age", "country"))
+	fmt.Println(sliceutil.GroupBy(employees, "country"))
 }
 
 func ruleCheck() {
--- sliceutil/sliceutil.go
+++ sliceutil/sliceutil.go
@@ -1,6 +1,10 @@
 package sliceutil
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/goccy/go-reflect"
 	"github.com/samber/lo"
 )
 
@@ -26,4 +30,37 @@ func FilterMapByFieldValue(data []map[string]any, field string, value any) []map
 		}
 		return nil, false
 	})
+}
+
+// GroupBy is a generic function that groups items by a specified key.
+func GroupBy[T any](items []T, fieldNames ...string) map[string][]T {
+	var t T
+	kind := reflect.ValueOf(t).Kind()
+	grouped := make(map[string][]T)
+	keys := make([]string, len(fieldNames))
+	for _, item := range items {
+		switch kind {
+		case reflect.Struct:
+			itemValue := reflect.ValueOf(item)
+			for i, fieldName := range fieldNames {
+				fieldValue := itemValue.FieldByName(fieldName)
+				keys[i] = fmt.Sprint(fieldValue.Interface())
+			}
+		case reflect.Map:
+			if m, ok := any(item).(map[string]any); ok {
+				for i, fieldName := range fieldNames {
+					if val, ok := m[fieldName]; ok {
+						keys[i] = fmt.Sprint(val)
+					}
+				}
+			}
+		}
+
+		k := strings.Join(keys, "_")
+		if _, ok := grouped[k]; !ok {
+			grouped[k] = make([]T, 0, len(items)/len(keys))
+		}
+		grouped[k] = append(grouped[k], item)
+	}
+	return grouped
 }
